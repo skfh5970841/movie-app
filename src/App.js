b5692c6a5efa856ff4331311cc76a71d64bceb4e
skfh5970841 <leechangwan1012@gmail.com>
2020-02-10 16:25:10 +0900

    page_commited

--- src/App.js
+++ src/App.js
@@ -1,45 +1,48 @@
 import React from 'react';
-function Food({name, picture}){
-    return (
-        <div>
-            <h1> I like {name}</h1>
-            <img src={picture} />
-        </div>      
-    );
-}
+import PropTypes from "prop-types";
+import axios from "axios";
+import Movie from "./Movie";
+import "./Style/App.css";
 
-const foodILike = [
-    {
-        name : "samgupsal",
-        image : "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2F7%2F77%2FSamgyeopsal-gui.jpg%2F1200px-Samgyeopsal-gui.jpg&f=1&nofb=1"
-    },
-    {
-        name : "babeku",
-        image : "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fnews.warwickshire.gov.uk%2Fwp-content%2Fuploads%2F2012%2F05%2FBBQ.jpg&f=1&nofb=1"
-    },
-    {
-        name : "pokcap",
-        image : "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2F6%2F64%2FPork_chops_served.jpg%2F1200px-Pork_chops_served.jpg&f=1&nofb=1"
-    },
-    {
-        name : "boccam",
-        image : "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimage.fmkorea.com%2Ffiles%2Fattach%2Fnew%2F20170508%2F486616%2F38307501%2F647835939%2F5a36febeaa11240030120a3f628aa65d.jpg&f=1&nofb=1"
-
-    },
-    {
-        name : "jokbal",
-        image : "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fcfile208.uf.daum.net%2Fimage%2F11678E4B4D8ABF2A28F9C2&f=1&nofb=1"
+class App extends React.Component{
+    state = {
+        isLoding : true,
+        movies : []
     }
-];
-
-function App() {
-    return ( 
-        <div> 
-            {
-                foodILike.map((dish)=> <Food name={dish.name} picture={dish.image}/>)
-            }
-        </div>
-    );
+    getMovies = async() =>{
+        const {data : { data : { movies }}} = await axios.get("https://yts.mx/api/v2/list_movies.json?sort_by=rating");
+        this.setState({ movies , isLoding : false});
+    }
+    async componentDidMount(){
+        this.getMovies();
+    }
+    render(){
+        const {isLoding, movies} = this.state;
+        return <section class="container">{
+            isLoding 
+            ? <div class="loader">
+                <span class="loader__text">Loading</span>
+            </div> 
+            : (
+                <div class="movies">
+                    {
+                        movies.map(
+                            movie => (
+                            <Movie 
+                            key={movie.id} 
+                            id={movie.id} 
+                            year={movie.year} 
+                            title={movie.title} 
+                            summary={movie.summary} 
+                            poster={movie.medium_cover_image}
+                            genres={movie.genres}
+                            />
+                        ))
+                    }
+                </div>
+        )}</section>;
+    }
 }
+
 
 export default App;
